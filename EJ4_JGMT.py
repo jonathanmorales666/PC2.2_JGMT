c2da50a138d45f64e4969b8921a138a832e5a782
Jonathan <jonathanmorales@emilianisomascos.edu.gt>
2026-04-09 11:03:44 -0600

    Ejercicio #4 terminado

---
+++ EJ4_JGMT.py
@@ -0,0 +1,8 @@
+# Jonathan Gabriel Morales Torres
+# Operadores Lógicos
+
+descuento_activado = False
+es_cliente_vip = True
+
+# Gana un cupón si tiene descuento activado 0 si es cliente VIP
+print("¿Obtiene cupón?", descuento_activado or es_cliente_vip) # True
