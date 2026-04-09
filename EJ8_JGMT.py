87d7edf064286c7911a1c767acdffdbd843932ee
Jonathan <jonathanmorales@emilianisomascos.edu.gt>
2026-04-09 11:10:53 -0600

    Ejercicio #8 terminado

---
+++ EJ8_JGMT.py
@@ -0,0 +1,7 @@
+# Jonathan Gabriel Morales Torres
+#Operadores de Pertenencia
+
+mensaje = "Bienvenido a la clase de Python"
+
+print("¿La palabra 'Python' esta en el mensaje?", "Python" in mensaje)
+print("¿La palabra 'Java' está en el mensaje?", "Java" not in mensaje)
