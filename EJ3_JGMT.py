f888e31e7acaf1729efb579fe2c777e70ae55a27
Jonathan <jonathanmorales@emilianisomascos.edu.gt>
2026-04-09 11:01:40 -0600

    Se añadieron el resto de plantillas para los ejercicios 3-9, ejercicio #3 terminado

---
+++ EJ3_JGMT.py
@@ -0,0 +1,10 @@
+# Jonathan Gabriel Morales Torres 10
+# Operadores Logicos
+
+
+edad = 18
+
+tiene_entrada = True
+
+# Solo puede entrar si tiene más de 17 años Y tiene entrada
+print("¿Puede ingresar al evento?", edad > 17 and tiene_entrada)
