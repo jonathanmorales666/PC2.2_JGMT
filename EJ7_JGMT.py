e5381b46129561a066209c97b75241807401920e
Jonathan <jonathanmorales@emilianisomascos.edu.gt>
2026-04-09 11:09:31 -0600

    Ejercicio #7 terminado

---
+++ EJ7_JGMT.py
@@ -0,0 +1,7 @@
+# Jonathan Gabriel Morales Torres 10
+# Operadores de Pertenencia
+
+frutas = ["manzana", "pera", "sandía"]
+
+print("¿'pera' está en la lista?", "pera" in frutas)
+print("¿'melón' está en la lista?", "melón" in frutas)
