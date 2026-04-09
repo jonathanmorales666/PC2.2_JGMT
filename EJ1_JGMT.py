b7c14df5bfe9d4ebc9f93b8d379213706ac185bb
Jonathan <jonathanmorales@emilianisomascos.edu.gt>
2026-04-09 10:52:05 -0600

    Primer ejercicio terminado

---
+++ EJ1_JGMT.py
@@ -0,0 +1,10 @@
+# Jonathan Gabriel Morales Torres 10
+# Operadores de Comparación
+
+# Edad de dos personas
+edad_juan = 20
+edad_maria = 25
+
+# Comparamos las edades
+print("¿Juan tiene la misma edad que María?", edad_juan == edad_maria)
+print("¿Juan es menor que María?", edad_juan < edad_maria)
